c8e5bda3cd9d79afd14d8ff2ba7c75493f0a9d01
Eiman Salek <eimanabde04@gmail.com>
2024-11-13 22:58:29 +0100

    Metodo en el DAO para obtener todos los animales

--- src/main/java/com/eiman/ejs/dao/AnimalDAO.java
+++ src/main/java/com/eiman/ejs/dao/AnimalDAO.java
@@ -22,6 +22,38 @@ public class AnimalDAO {
     }
 
     /**
+     * Obtiene todos los registros de animales en la base de datos.
+     *
+     * @return Lista de animales.
+     * @throws SQLException Si ocurre un error en la base de datos.
+     */
+    public List<Animal> getAllAnimals() throws SQLException {
+        List<Animal> animals = new ArrayList<>();
+        String query = "SELECT * FROM animals";
+
+        try (Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(query)) {
+
+            while (rs.next()) {
+                Animal animal = new Animal(
+                        rs.getInt("id"),
+                        rs.getString("nombre"),
+                        rs.getString("especie"),
+                        rs.getString("raza"),
+                        rs.getString("sexo"),
+                        rs.getInt("edad"),
+                        rs.getDouble("peso"),
+                        rs.getString("observaciones"),
+                        rs.getString("fecha_primera_consulta"),
+                        rs.getBytes("foto")
+                );
+                animals.add(animal);
+            }
+        }
+        return animals;
+    }
+
+    /**
      * Cierra la conexion a la base de datos.
      *
      * @throws SQLException Si ocurre un error al cerrar la conexion.
